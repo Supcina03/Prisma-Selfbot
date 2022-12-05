eb86f563d8ee2a1000c8f1f11da9e201a4783535
Nocas-sam-u-krizi <88481220+Nocas-sam-u-krizi@users.noreply.github.com>
2022-12-05 14:27:15 +0100

    2 files modified, 1 added

--- src/app/tutorial/links.component.ts
+++ src/app/tutorial/links.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-links',
   templateUrl: './links.component.html',
+  styleUrls: ['./links.component.css'],
 })
 export class LinksComponent {
   links = [
