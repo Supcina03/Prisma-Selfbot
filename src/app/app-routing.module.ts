0ac5640c5a9c2e3c6493330cd2cea5abdb22389c
Nocas-sam-u-krizi <88481220+Nocas-sam-u-krizi@users.noreply.github.com>
2022-12-05 10:59:24 +0100

    1 binaries uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ExampleComponent } from './example/example.component';
 
 const routes: Routes = [
   {
-    path: '/login',
+    path: '',
+    component: ExampleComponent,
+  },
+  {
+    path: 'login',
     component: LoginComponent,
   },
 ];
