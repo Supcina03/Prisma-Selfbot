73545087775755b09d6b45531e8aa4ced693b962
Nocas-sam-u-krizi <88481220+Nocas-sam-u-krizi@users.noreply.github.com>
2022-12-05 14:46:58 +0100

    1 file modified, 1 added

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css'],
 })
 export class ExampleComponent {}
